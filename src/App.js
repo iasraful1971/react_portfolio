a65fc9110ce8e24fb564c7a0d204ec7eefbdada9
asraful <iasraful107@gmail.com>
2022-01-06 21:39:36 +0600

    addded scroll to top

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 
-import MessengerCustomerChat from 'react-messenger-customer-chat';
+// import MessengerCustomerChat from 'react-messenger-customer-chat';
+import FloatingWhatsApp from 'react-floating-whatsapp';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from '../src/components/Home/Home';
 import './App.css';
 import About from './components/About/About';
 import ArgumnetHome from './components/Argument/ArgumnetHome/ArgumnetHome';
+import BackToTop from './components/BackToTop/BackToTop';
 import Blog from './components/Blog/Blog';
 import Contact from './components/Contact/Contact';
 import Footer from './components/Footer/Footer';
 import NavbarOne from './components/navbarOne/NavbarOne';
 import Portfolio from './components/Portfolio/portfolio/Portfolio';
 import Privency from './components/Privency/Privency';
+import ScrollToTop from './components/ScrollToTop';
+import photo from './images/asraful style picture.png';
 function App() {
   return (
     <div className="App">
-        <BrowserRouter>
-        
 
-    <NavbarOne></NavbarOne>
+        <BrowserRouter>
+          <BackToTop/>
+           <ScrollToTop/>
+           <NavbarOne></NavbarOne>
 
         <Switch>
     
@@ -48,11 +53,18 @@ function App() {
         </Switch>
         <Footer/>
           </BrowserRouter>
-          <MessengerCustomerChat
+          {/* <MessengerCustomerChat
           pageId="102089495265783"
-           appId="1234412133752549"
-        
-  />
+           appId="1234412133752549"/> */}
+            <FloatingWhatsApp 
+        phoneNumber="+8801633780292" 
+        accountName="Muhammad Asraful"
+        avatar={photo}
+        statusMessage="Typically replies within 1 hour"
+        chatMessage="How can I help you Darling? "
+        darkMode= {true}
+        notification={true}
+        />
 
     </div>
   );
---
+++ src/components/BackToTop/BackToTop.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import { useWindowScroll } from 'react-use';
+import './BackToTop.css';
+const BackToTop = () => {
+   const { y : pageYOffset} = useWindowScroll();
+   const [visible, setVisible] = useState(false);
+
+    useEffect(() => {
+            if(pageYOffset > 400){
+                    setVisible(true)
+            }else{
+                setVisible(false)
+            }
+    }, [pageYOffset]);
+    
+const scrollToTop = () => window.scrollTo({
+    top:0, behavior: "smooth"
+})
+   if(!visible){
+        return false;
+   }
+
+    return (
+        <div className="scroll-top" onClick={scrollToTop}>
+            <i className="icon fas fa-chevron-up"></i>
+        </div>
+    );
+};
+
+export default BackToTop;
